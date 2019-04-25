7aaf5e814444c494d98c7632217d208acbd64651
Vlad <gokkatx@gmail.com>
2019-04-25 12:05:39 +0300

    это наш первый комит

---
+++ JS/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var a = c5 ;
+console.log(a);
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// // console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+let persone = {
+  name:"John",
+  age:25,
+  isMarried:false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orang.jpg', 'apple.bmp'];
+
+console.log(arr[0])
+
+// alert("Hello world!");
+
+// let answer = confirm("Are ou here?");
+
+// console.log(answer)
+// let answer = +prompt("есть ли вам ?", "да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log( 4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+let isChecked = false;
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(isChecked || !isClose);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let arr = ['plum.png', 'orang.jpg', 'apple.bmp'];
+
+console.log(arr[1])
